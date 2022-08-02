9b6c999e608180e53a7c7d064f29b4a29ca39ff4
DomParish <dom.l.parish@icloud.com>
2022-08-02 17:38:56 -0500

    -About changed to blog
    -Navbar updated

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -5,8 +5,10 @@ const Navbar = () => {
             <h1>Lipsum</h1>
         </div>
             <a>Home</a>
-            <a>About</a>
+            <a>Blog</a>
             <a>Apply</a>
+            <a>Leadership</a>
+            <a>Human Resources</a>
             <a>Front of House</a>
             <a>Kitchen</a>
 
--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,4 @@
-const About = () => {
+const Blog = () => {
         return (
             <div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
@@ -9,4 +9,4 @@ const About = () => {
 
 }
 
-export default About;
+export default Blog;
