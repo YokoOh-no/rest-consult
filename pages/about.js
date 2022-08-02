4ff0c8e57964a7ebdbf13f0ac3cf3008f4ae14b6
DomParish <dom.l.parish@icloud.com>
2022-08-02 00:30:03 -0500

    -Gutted temp root index
    -Add weather page
    
    -Add About page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+        return (
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
+            </div>
+        );
+
+
+}
+
+export default About;
---
+++ pages/weather.js
@@ -0,0 +1,12 @@
+const Weather = () => {
+    return (
+
+        <div>
+            <h1>Welcome to Weather</h1>
+
+        </div>
+
+    )
+
+}
+export default Weather
