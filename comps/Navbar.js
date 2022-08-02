7a00b4fe00fd89f5bcefc22adece91fb572d9459
DomParish <dom.l.parish@icloud.com>
2022-08-02 01:53:03 -0500

    -Navbar created but its broken

---
+++ comps/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+        return(
+            <nav>
+                <div className="logo">
+                    <h1>Lipsum</h1>
+                </div>
+                <div>
+                    <a>Home</a>
+                    <a>About</a>
+                    <a>Weather</a>
+                </div>
+            </nav>
+        );
+}
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return ( 
           <div>
+              <Navbar />
               <h1>Homepage</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
           </div>
  
   )
