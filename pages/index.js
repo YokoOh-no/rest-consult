adc3ae1360640c5238fe39198cdfcdfc9015ff7f
DomParish <dom.l.parish@icloud.com>
2022-08-03 04:55:44 -0500

    homepage html restart

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
+import {useSession, signIn, signOut } from "next-auth/client"; 
 import Head from 'next/head'
 import Footer from '../comps/footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Link from "next/link";
+
+
 
 export default function Home() {
-    return (<div>
-        
-        <h1>Homepage</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget magna fermentum iaculis eu. Est placerat in egestas erat imperdiet sed. Euismod </p>
-        <Link href="/apply-here"><a>Apply Here Now</a></Link>
-                 
-            </div>);
-}
+    const [session, loading]= useSession();
+    if (session) {
+    return (
+        <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign Out</button>
+
+        </>
+    );
+    }}
+    
