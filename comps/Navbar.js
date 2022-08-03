7d1aef26d7875d87166b4f953da776894696d70d
DomParish <dom.l.parish@icloud.com>
2022-08-02 21:39:49 -0500

    - CSS started

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -7,12 +7,12 @@ const Navbar = () => {
             <h1>Lipsum</h1>
         </div>
             <Link href="/"><a>Home</a></Link>
-            <Link href="/blog"><a>Blog</a></Link>
             <Link href="/apply-here"><a>Apply</a></Link>
+            <Link href="/blog"><a>Blog</a></Link>
             <Link href="/management"><a>Leadership</a></Link>
-            <Link href="/hr"><a>Human Resources</a></Link>
             <Link href="/foh"><a>Front of House</a></Link>
             <Link href="/boh"><a>Kitchen</a></Link>
+            <Link href="/hr"><a>Human Resources</a></Link>
 
     </nav>
 
